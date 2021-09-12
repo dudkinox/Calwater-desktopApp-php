c755099f8ea36a9f3a61b8c29e6c8710958058c4
pornpitak <77528292+HEEPOKE@users.noreply.github.com>
2021-09-13 01:11:21 +0700

    card

--- lib/dashboard/Dashboard.dart
+++ lib/dashboard/Dashboard.dart
@@ -23,64 +23,62 @@ class _DashboardState extends State<Dashboard> {
           Menu(),
           // Dashboard
           Card(
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(53)),
-            child: Padding(
-                padding: EdgeInsets.symmetric(
-                  vertical: 100,
-                  horizontal: 300,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Container(
-                      padding: new EdgeInsets.only(top: 16.0),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(53)),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 1155,
+                    child: Align(
+                      alignment: Alignment.topCenter,
                       child: Text(
                         "ระบบคำนวณระบบน้ำตัวอย่าง",
+                        textAlign: TextAlign.center,
                         style: TextStyle(
                           fontFamily: 'Itim',
                           fontSize: 48,
                         ),
                       ),
                     ),
-                    Row(
-                      children: [
-                        Container(
-                          child: Column(
-                            children: [
-                              Card(
-                                color: Colors.black,
-                                child: Text("textststst"),
-                              ),
-                            ],
-                          ),
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        child: Column(
+                          children: [
+                            Card(
+                              color: Colors.black,
+                              child: Text("textststst"),
+                            ),
+                          ],
                         ),
-                        Container(
-                          child: Column(
-                            children: [
-                              Card(
-                                color: Colors.black,
-                                child: Text("textststst"),
-                              ),
-                            ],
-                          ),
+                      ),
+                      Container(
+                        child: Column(
+                          children: [
+                            Card(
+                              color: Colors.black,
+                              child: Text("textststst"),
+                            ),
+                          ],
                         ),
-                        Container(
-                          child: Column(
-                            children: [
-                              Card(
-                                color: Colors.black,
-                                child: Text("textststst"),
-                              ),
-                            ],
-                          ),
+                      ),
+                      Container(
+                        child: Column(
+                          children: [
+                            Card(
+                              color: Colors.black,
+                              child: Text("textststst"),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                  ],
-                )),
-          ),
+                      ),
+                    ],
+                  ),
+                ],
+              )),
         ],
       ),
     );
