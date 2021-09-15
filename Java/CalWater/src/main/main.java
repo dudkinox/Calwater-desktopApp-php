18c8c49b237d34ece1ed7c916a8ebf08747440bd
ศุภณัฐ ชินราช <45527907+dudkinox@users.noreply.github.com>
2021-09-16 01:24:59 +0700

    Upload Java project

---
+++ Java/CalWater/src/main/main.java
@@ -0,0 +1,53 @@
+package main;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JSplitPane;
+import javax.swing.JToolBar;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+
+public class main extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					main frame = new main();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public main() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(350, 100, 1280, 768);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(177, 177, 177));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon("E:\\Flutter\\flutter_desktopapp\\assets\\icon\\logo.png"));
+		lblNewLabel.setBounds(0, 0, 183, 156);
+		contentPane.add(lblNewLabel);
+	}
+}
