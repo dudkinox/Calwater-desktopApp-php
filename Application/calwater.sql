6d0c32ed01046f551cdf258053548b4748cf79b7
ศุภณัฐ ชินราช <45527907+dudkinox@users.noreply.github.com>
2021-10-07 18:35:31 +0700

    Update calwater.sql

--- Application/calwater.sql
+++ Application/calwater.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 06, 2021 at 10:11 AM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 8.0.10
+-- Generation Time: Oct 07, 2021 at 01:34 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `ca`
+--
+
+CREATE TABLE `ca` (
+  `model` decimal(50,2) NOT NULL,
+  `lab` decimal(50,2) NOT NULL,
+  `No` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_nopad_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fe`
+--
+
+CREATE TABLE `fe` (
+  `model` decimal(50,2) NOT NULL,
+  `lab` decimal(50,2) NOT NULL,
+  `No` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_nopad_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `info`
 --
 
@@ -34,89 +58,58 @@ CREATE TABLE `info` (
   `Meet_MOI_Spec` varchar(100) COLLATE utf8mb4_unicode_nopad_ci NOT NULL,
   `Sump_No` varchar(100) COLLATE utf8mb4_unicode_nopad_ci NOT NULL,
   `Treatment_Date` varchar(100) COLLATE utf8mb4_unicode_nopad_ci NOT NULL,
-  `RWWA_ID` varchar(100) COLLATE utf8mb4_unicode_nopad_ci NOT NULL,
-  `TWA_ID` varchar(100) COLLATE utf8mb4_unicode_nopad_ci NOT NULL
+  `Ca` int(10) NOT NULL,
+  `Fe` int(10) NOT NULL,
+  `Na` int(10) NOT NULL,
+  `TDS` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_nopad_ci;
 
 --
 -- Dumping data for table `info`
 --
 
-INSERT INTO `info` (`ID`, `Volume`, `Batch_No`, `Meet_MOI_Spec`, `Sump_No`, `Treatment_Date`, `RWWA_ID`, `TWA_ID`) VALUES
-(1, '1.000000000', 'S2202101', 'YES', 'S2', '09/22/2021', '1', '1'),
-(2, '1.000000000', 'S3202101', 'YES', 'S3', '09/22/2021', '2', '2'),
-(3, '1.000000000', 'E1202101', 'YES', 'E1', '09/22/2021', '3', '3'),
-(4, '0.123456789', 'S2202102', 'YES', 'S2', '09/22/2021', '4', '4'),
-(5, '0.123456789', 'S3202102', 'YES', 'S3', '09/22/2021', '5', '5'),
-(6, '0.123456789', 'E1202102', 'YES', 'E1', '09/22/2021', '6', '6'),
-(7, '1.000000000', 'S2202103', 'YES', 'S2', '09/23/2021', '7', '7'),
-(8, '1.000000000', 'S2202104', 'YES', 'S2', '10/04/2021', '8', '8');
+INSERT INTO `info` (`ID`, `Volume`, `Batch_No`, `Meet_MOI_Spec`, `Sump_No`, `Treatment_Date`, `Ca`, `Fe`, `Na`, `TDS`) VALUES
+(1, '10.000000000', 'S2202101', 'YES', 'S2', '10/07/2021', 1, 1, 1, 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `raw_waste_water_analysis`
+-- Table structure for table `na`
 --
 
-CREATE TABLE `raw_waste_water_analysis` (
-  `ID` int(100) NOT NULL,
-  `Total_Cr` decimal(50,9) NOT NULL,
-  `Cu` decimal(50,9) NOT NULL,
-  `Mn` decimal(50,9) NOT NULL,
-  `Ni` decimal(50,9) NOT NULL,
-  `Pb` decimal(50,9) NOT NULL,
-  `Zn` decimal(50,9) NOT NULL,
-  `TDS` int(100) NOT NULL
+CREATE TABLE `na` (
+  `model` decimal(50,2) NOT NULL,
+  `lab` decimal(50,2) NOT NULL,
+  `No` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_nopad_ci;
-
---
--- Dumping data for table `raw_waste_water_analysis`
---
-
-INSERT INTO `raw_waste_water_analysis` (`ID`, `Total_Cr`, `Cu`, `Mn`, `Ni`, `Pb`, `Zn`, `TDS`) VALUES
-(1, '2.000000000', '3.000000000', '4.000000000', '5.000000000', '6.000000000', '7.000000000', 0),
-(2, '2.000000000', '3.000000000', '4.000000000', '5.000000000', '6.000000000', '7.000000000', 0),
-(3, '2.000000000', '3.000000000', '4.000000000', '5.000000000', '6.000000000', '7.000000000', 0),
-(4, '0.123456789', '0.123456789', '0.123456789', '0.123456789', '0.123456789', '0.123456789', 0),
-(5, '0.123456789', '0.123456789', '0.123456789', '0.123456789', '0.123456789', '0.123456789', 0),
-(6, '0.123456789', '0.123456789', '0.123456789', '0.123456789', '0.123456789', '0.123456789', 0),
-(7, '2.000000000', '3.000000000', '4.000000000', '5.000000000', '6.000000000', '7.000000000', 0),
-(8, '2.000000000', '3.000000000', '4.000000000', '5.000000000', '6.000000000', '7.000000000', 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `treat_water_analysis`
+-- Table structure for table `tds`
 --
 
-CREATE TABLE `treat_water_analysis` (
-  `ID` int(100) NOT NULL,
-  `Total_Cr:0.25` decimal(50,9) NOT NULL,
-  `Cu:2.0` decimal(50,9) NOT NULL,
-  `Mn:5.0` decimal(50,9) NOT NULL,
-  `Ni:1.0` decimal(50,9) NOT NULL,
-  `Pb:0.2` decimal(50,9) NOT NULL,
-  `Zn:5.0` decimal(50,9) NOT NULL,
-  `TDS:None` int(100) NOT NULL
+CREATE TABLE `tds` (
+  `model` decimal(50,2) NOT NULL,
+  `lab` decimal(50,2) NOT NULL,
+  `No` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_nopad_ci;
-
---
--- Dumping data for table `treat_water_analysis`
---
-
-INSERT INTO `treat_water_analysis` (`ID`, `Total_Cr:0.25`, `Cu:2.0`, `Mn:5.0`, `Ni:1.0`, `Pb:0.2`, `Zn:5.0`, `TDS:None`) VALUES
-(1, '2.000000000', '3.000000000', '4.000000000', '5.000000000', '6.000000000', '7.000000000', 0),
-(2, '2.000000000', '3.000000000', '4.000000000', '5.000000000', '6.000000000', '7.000000000', 0),
-(3, '2.000000000', '3.000000000', '4.000000000', '5.000000000', '6.000000000', '7.000000000', 0),
-(4, '0.123456789', '0.123456789', '0.123456789', '0.123456789', '0.123456789', '0.123456789', 0),
-(5, '0.123456789', '0.123456789', '0.123456789', '0.123456789', '0.123456789', '0.123456789', 0),
-(6, '0.123456789', '0.123456789', '0.123456789', '0.123456789', '0.123456789', '0.123456789', 0),
-(7, '2.000000000', '3.000000000', '4.000000000', '5.000000000', '6.000000000', '7.000000000', 0),
-(8, '2.000000000', '3.000000000', '4.000000000', '5.000000000', '6.000000000', '7.000000000', 0);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `ca`
+--
+ALTER TABLE `ca`
+  ADD PRIMARY KEY (`No`);
+
+--
+-- Indexes for table `fe`
+--
+ALTER TABLE `fe`
+  ADD PRIMARY KEY (`No`);
 
 --
 -- Indexes for table `info`
@@ -125,10 +118,16 @@ ALTER TABLE `info`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Indexes for table `treat_water_analysis`
+-- Indexes for table `na`
 --
-ALTER TABLE `treat_water_analysis`
-  ADD PRIMARY KEY (`ID`);
+ALTER TABLE `na`
+  ADD PRIMARY KEY (`No`);
+
+--
+-- Indexes for table `tds`
+--
+ALTER TABLE `tds`
+  ADD PRIMARY KEY (`No`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -138,7 +137,7 @@ ALTER TABLE `treat_water_analysis`
 -- AUTO_INCREMENT for table `info`
 --
 ALTER TABLE `info`
-  MODIFY `ID` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `ID` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
